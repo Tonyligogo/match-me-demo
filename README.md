47690143d48f76033b0bb5aa9b854c592a6cecd9
EricTech0 <techwitheric@gmail.com>
2024-08-23 13:11:16 -0700

    Lesson 1 - NextJS basic app setup

---
+++ README.md
@@ -0,0 +1,17 @@
+# Match Me App
+
+> App for people to find their match.
+
+## URL
+
+[YouTube App Demo](?)
+
+## Tools
+
+- NextJS
+- Prisma ORM
+- TailwindCSS
+- NextUI
+- NextAuth
+- TypeScript
+- React
---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+import { Button, Link } from "@nextui-org/react";
+import { FaRegSmile } from "react-icons/fa";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl">Hello app!</h1>
+      <Button
+        as={Link}
+        href="/members"
+        color="primary"
+        variant="bordered"
+        startContent={<FaRegSmile size={20} />}
+      >
+        Click me!
+      </Button>
+    </div>
+  );
+}
