16e36af02e2396bbdb9049d7c4dae4397bb43399
EricTech0 <techwitheric@gmail.com>
2024-08-29 16:07:30 -0700

    Lesson 3 - Add User Authentication

--- src/components/navbar/TopNav.tsx
+++ src/components/navbar/TopNav.tsx
@@ -8,8 +8,11 @@ import Link from "next/link";
 import React from "react";
 import { GiSelfLove } from "react-icons/gi";
 import NavLink from "./NavLink";
+import { auth } from "@/auth";
+import UserMenu from "./UserMenu";
 
-export default function TopNav() {
+export default async function TopNav() {
+  const session = await auth();
   return (
     <Navbar
       maxWidth="full"
@@ -46,22 +49,28 @@ export default function TopNav() {
         />
       </NavbarContent>
       <NavbarContent justify="end">
-        <Button
-          as={Link}
-          href="/login"
-          variant="bordered"
-          className="text-white"
-        >
-          Login
-        </Button>
-        <Button
-          as={Link}
-          href="/register"
-          variant="bordered"
-          className="text-white"
-        >
-          Register
-        </Button>
+        {session?.user ? (
+          <UserMenu user={session.user} />
+        ) : (
+          <>
+            <Button
+              as={Link}
+              href="/login"
+              variant="bordered"
+              className="text-white"
+            >
+              Login
+            </Button>
+            <Button
+              as={Link}
+              href="/register"
+              variant="bordered"
+              className="text-white"
+            >
+              Register
+            </Button>
+          </>
+        )}
       </NavbarContent>
     </Navbar>
   );
