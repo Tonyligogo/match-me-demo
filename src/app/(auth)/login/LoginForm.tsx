802e5a2bd2e4317b903d5b81803b66768473ce6b
Tony Ligogo <tonyligogo@gmail.com>
2025-06-02 17:12:10 +0300

    disabled google signin

--- src/app/(auth)/login/LoginForm.tsx
+++ src/app/(auth)/login/LoginForm.tsx
@@ -87,12 +87,12 @@ export default function LoginForm() {
             >
              {loading ? "" : "Login"}
             </Button>
-            <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
+            {/* <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
           <span className="relative z-10 bg-background px-2 text-muted-foreground">
             Or continue with
           </span>
-        </div>
-            <SocialLogin />
+        </div> */}
+            {/* <SocialLogin /> */}
             <div className="text-center text-sm">
         Don&apos;t have an account?{" "}
         <Link href="/register" className="underline underline-offset-4">
