3ee023dc10b175a3a366a4c0dfbd406c6852d87e
Tony Ligogo <tonyligogo@gmail.com>
2025-06-05 14:21:47 +0300

    landing page text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 export default async function Home() {
 
   return (
-    <div className="flex flex-col justify-center items-center mt-20 gap-6 text-default">
-      <h1 className="text-4xl font-bold">
+    <div className="flex flex-col justify-center items-center vertical-center gap-6">
+      <h1 className="text-4xl text-center text-primaryBlue font-bold">
         Welcome to Starmingle
       </h1>
     </div>
